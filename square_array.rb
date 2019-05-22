40f0e1e287afa13531dce8e77aaa47fba7026502
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 03:08:59 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |number|
+    squared_number = number**2
+    new_array.push(squared_number)
+  end
+  return new_array
 end
